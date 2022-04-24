2b407455790b3ed1d3662877e7b04ec3a2e54bae
andreaminolfi <andrea@minolfi.it>
2022-04-24 18:23:32 +0200

    1 file modified

--- src/app/pages/comments.component.ts
+++ src/app/pages/comments.component.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-comments',
   template: `
-    I'm the comments page
+    <h1>Comments</h1>
+    <!--<pre>{{comments | json}}</pre>-->
+    
+    <li *ngFor="let comment of comments">
+      {{comment.body}}
+    </li>
+
   `,
 })
-export class CommentsComponent {}
+export class CommentsComponent {
+  comments: Comment[] = [];
+
+  constructor(activatedRoute: ActivatedRoute, http: HttpClient) {
+    const postId = activatedRoute.snapshot.params.postId;
+    http
+      .get<Comment[]>(
+        `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+      )
+      .subscribe((res) => (this.comments = res));
+  }
+}
