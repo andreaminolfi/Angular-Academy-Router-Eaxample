fd34c09268b571c99713f24013f533544c2c1620
andreaminolfi <andrea@minolfi.it>
2022-04-24 18:36:40 +0200

    2 files modified

--- src/app/pages/comments.component.ts
+++ src/app/pages/comments.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PostComment } from '../model/comment';
+import { Posts } from '../model/posts';
 
 @Component({
   selector: 'app-comments',
@@ -8,6 +10,8 @@ import { ActivatedRoute } from '@angular/router';
     <h1>Comments</h1>
     <!--<pre>{{comments | json}}</pre>-->
     
+    <h2>{{posts.title}}</h2>
+
     <li *ngFor="let comment of comments">
       {{comment.body}}
     </li>
@@ -15,12 +19,16 @@ import { ActivatedRoute } from '@angular/router';
   `,
 })
 export class CommentsComponent {
-  comments: Comment[] = [];
+  comments: PostComment[] = [];
+  posts: Posts = null;
 
   constructor(activatedRoute: ActivatedRoute, http: HttpClient) {
     const postId = activatedRoute.snapshot.params.postId;
     http
-      .get<Comment[]>(
+      .get<Posts>(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .subscribe((res) => (this.posts = res));
+    http
+      .get<PostComment[]>(
         `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
       )
       .subscribe((res) => (this.comments = res));
